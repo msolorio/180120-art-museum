50bf220fe92f948fca8c0d2fbdc7a9c238eae60f
msolorio <msolorio789@gmail.com>
2018-01-21 12:05:41 -0800

    use Schema constructor

---
+++ prompt/schemas/Schema.js
@@ -0,0 +1,21 @@
+/**
+* Schema - constructor for a prompt schema.
+*
+* @param  {array} props - takes in array of properties to be requested
+* on the prompt
+* @return {type} returns an object specifying prompt property
+names, validation messages, and sets all properties to required
+*/
+function Schema(props) {
+  this.properties = {};
+
+  props.forEach((prop) => {
+    this.properties[prop] = {
+      description: prop,
+      message: `${prop} was not provided`,
+      required: true
+    };
+  });
+}
+
+module.exports = Schema;
--- prompt/schemas/addItemSchema.js
+++ prompt/schemas/addItemSchema.js
@@ -1,26 +1,12 @@
-const addItemSchema = {
-  properties: {
-    artist: {
-      description: 'artist',
-      message: 'artist name not provided',
-      required: true
-    },
-    title: {
-      description: 'title',
-      message: 'title not provided',
-      required: true
-    },
-    year: {
-      description: 'year',
-      message: 'year not provided',
-      required: true
-    },
-    medium: {
-      description: 'medium',
-      message: 'medium not provided',
-      required: true
-    }
-  }
-};
+const Schema = require('./Schema');
+
+const props = [
+  'artist',
+  'title',
+  'year',
+  'medium'
+];
+
+const addItemSchema = new Schema(props);
 
 module.exports = addItemSchema;
