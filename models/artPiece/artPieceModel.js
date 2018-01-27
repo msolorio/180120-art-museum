5d7f15c7fbe1dffe2bbe2c604512c7b0df057f4f
msolorio <msolorio789@gmail.com>
2018-01-27 14:31:34 -0800

    remove unused method

--- models/artPiece/artPieceModel.js
+++ models/artPiece/artPieceModel.js
@@ -4,17 +4,12 @@ const inputtedArtPieceProps = rootRequire('./models/artPiece/inputtedArtPiecePro
 
 function ArtPiece(config) {
   if (config._id) this._id = config._id;
-  
+
   else this._id = generateRandomHash();
 
   inputtedArtPieceProps.forEach((prop) => {
     this[prop] = config[prop];
   });
 }
-
-// TODO: implement update functionality via prototype method
-ArtPiece.prototype.update = function() {
-  console.log('in ArtPiece.prototype.update');
-};
 
 module.exports = ArtPiece;
